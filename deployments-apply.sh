14717e53d4258f500b13b592ddf2fb4493d6cf36
Eero Tamminen <eero.t.tamminen@intel.com>
2023-01-23 14:14:14 +0200

    Add pod state checks to "deployments-apply.sh"
    Use the wait_pod_state() function from "validate-scaling.sh" to wait
    (with a fail timeout) until required deployment state count is reached
    for the (most relevant) frontend & backend pods in monitoring
    namespace.
    
    And add separator comments for different phases of the script.
    
    I'm keeping wait_pod_state() identical in both scripts in case it
    would later be split to its own helper script.
    
    Signed-off-by: Eero Tamminen <eero.t.tamminen@intel.com>

--- deployments-apply.sh
+++ deployments-apply.sh
@@ -4,8 +4,9 @@
 
 set -e
 
-# avoid error on missing objects
-delete="kubectl delete --ignore-not-found=true"
+# ---------------- option / arg checking ------------------
+
+SEPARATOR="------------------------------------------------"
 
 usage ()
 {
@@ -39,6 +40,12 @@ if [ ! -d deployments ]; then
 fi
 cd deployments
 
+FRONTEND=frontend/frontend.yaml
+NAMESPACE=$(awk '/^ *namespace:/{print $2}' $FRONTEND)
+if [ -z "$NAMESPACE" ]; then
+	usage "unable to determine '$FRONTEND' namespace"
+fi
+
 del_vols="false"
 if [ "$1" = "-v" ]; then
 	del_vols="true"
@@ -51,12 +58,72 @@ for name in "$@"; do
 	fi
 done
 
-echo "Removing all scalability tester deployments..."
+# ---------------- pod state helper ------------------
+
+# wait upto given timeout until specified number of pods with given prefix are in given state
+# in given namespace, and none of those pods are in any other state
+wait_pod_state ()
+{
+	SECS=$1
+	COUNT=$2
+	PREFIX=$3
+	SPACE=$4
+	STATE=$5
+
+	echo "Waiting ${SECS}s for $COUNT $SPACE/${PREFIX}* deployment(s) to be in '$STATE' state..."
+
+	SLEEP=2
+	while [ "$SECS" -gt 0 ]; do
+		# start with sleep to make sure terminated pods are not anymore in running state
+		SECS=$((SECS-SLEEP))
+		sleep $SLEEP
+
+		# show current deployment status, with node names
+		kubectl -n "$SPACE" get pods -o wide | grep -F "^$PREFIX" || true
+
+		# requested number of deployments not yet in given state?
+		LINES=$(kubectl -n "$SPACE" get pods | grep -c "^${PREFIX}.* $STATE " || true)
+		echo "= $LINES/$COUNT pods (${SECS}s remaining)"
+		if [ "$LINES" -ne "$COUNT" ]; then
+			continue
+		fi
+
+		# none in any other state?
+		LINES=$(kubectl -n "$SPACE" get pods | grep -F "^$PREFIX" | grep -vcF " $STATE " || true)
+		if [ "$LINES" -eq 0 ]; then
+			# => wait done
+			return
+		break
+		echo "$LINES pods still in other state"
+	fi
+	done
+
+	echo "ERROR: deployment wait timed out"
+
+	# on timeout, show log for first erroring pod
+	CRASHER=$(kubectl -n "$SPACE" get pods | awk "/^${PREFIX}.* (Crash|Error)/"'{print $1; exit}')
+	if [ "$CRASHER" != "" ]; then
+		echo $SEPARATOR
+		echo "kubectl -n $SPACE logs $CRASHER"
+		kubectl -n "$SPACE" logs "$CRASHER"
+		echo $SEPARATOR
+	fi
+	exit 1
+}
+
+# ---------------- previous deployment removal ------------------
+
+# avoid error on missing objects
+delete="kubectl delete --ignore-not-found=true"
+
+echo "Removing current scalability tester deployments..."
 
 for queue in *-queue; do
 	name=${queue%-queue}
 	echo "Deleting '$name' queue deployment..."
 	$delete -f "$queue"
+	# secs count prefix space state
+	wait_pod_state 20 0 "scalability-tester-backend-$name-" "$NAMESPACE" "Running"
 done
 
 # need to be done after all queues have been deleted,
@@ -72,12 +139,16 @@ done
 echo "Deleting frontend deployment / service..."
 $delete -f frontend/
 $delete -f frontend/service/
+# secs count prefix space state
+wait_pod_state 20 0 "scalability-tester-frontend-" "$NAMESPACE" "Running"
 
 echo
 if [ $# -lt 1 ]; then
 	echo "No queues specified -> DONE!"
 	exit 0
 fi
+
+# ---------------- create specified deployment ------------------
 
 # as these may be used also for other purposes, do not
 # remove them, and use apply instead of create
@@ -89,6 +160,8 @@ kubectl apply -f namespace
 echo "Creating frontend..."
 kubectl create -f frontend/service/
 kubectl create -f frontend/
+# secs count prefix space state
+wait_pod_state 60 1 "scalability-tester-frontend-" "$NAMESPACE" "Running"
 
 echo "Starting specified queues..."
 for name in "$@"; do
@@ -100,12 +173,13 @@ for name in "$@"; do
 	fi
 	echo "Create '$name' queue deployment..."
 	kubectl create -f "$queue"
+	wait_pod_state 60 1 "scalability-tester-backend-$name-" "$NAMESPACE" "Running"
 done
 
 awk '/name:/{print $2}' namespace/*.yaml | while read -r ns; do
 	echo
 	echo "Resulting '$ns' namespace deployments:"
-	echo "  kubectl -n "$ns" get all | grep -e NAME -e scalability"
+	echo "  kubectl -n $ns get all | grep -e NAME -e scalability"
 	echo
 	kubectl -n "$ns" get all | grep -e NAME -e scalability
 done
