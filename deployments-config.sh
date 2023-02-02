0df27e2fbe0cfd732b638df870d2663b87c8c2b2
Eero Tamminen <eero.t.tamminen@intel.com>
2023-02-02 19:19:04 +0200

    Improve namespace + registry URI name checks
    Separate checks for their names so that namespace check is restricted
    only to valid namespace chars, and add ':' to accepted registry URI
    chars.
    
    Reported-By: Tuomas Katila <tuomas.katila@intel.com>
    
    Signed-off-by: Eero Tamminen <eero.t.tamminen@intel.com>

--- deployments-config.sh
+++ deployments-config.sh
@@ -16,7 +16,7 @@ usage ()
 	echo "Usage: $name REGISTRY PROM_NS TEST_NS"
 	echo
 	echo "Script changes following items for the deployments:"
-	echo "- REGISTRY: registry/project for container images"
+	echo "- REGISTRY: registry/project URI prefix for container images"
 	echo "- PROM_NS: namespace where deployments providing Prometheus metrics should run"
 	echo "- TEST_NS: namespace where rest of the deployments should run"
 	echo
@@ -44,16 +44,25 @@ if [ $# -ne 3 ]; then
 	usage "incorrect number of arguments"
 fi
 
+# check whether registry domain [:port] / subpath includes only valid chars:
+# https://github.com/distribution/distribution/blob/main/docs/spec/api.md#overview
+invalid=$(echo "$1" | tr -d a-z0-9/:_.-)
+if [ -n "$invalid" ]; then
+	usage "suspicious registry name / path '$1' specified (includes '$invalid' chars)"
+fi
+REGISTRY=$1
+shift
+
+# check whether namespace names include only valid chars:
+# https://kubernetes.io/docs/concepts/overview/working-with-objects/names/
 for i in "$@"; do
-	invalid=$(echo "$i" | tr -d a-zA-Z0-9/_.-)
+	invalid=$(echo "$i" | tr -d a-z0-9-)
 	if [ -n "$invalid" ]; then
-		usage "'$i' include invalid chars ('$invalid') for a valid namespace or registry name"
+		usage "'$i' name includes invalid chars ('$invalid') for a namespace"
 	fi
 done
-
-REGISTRY=$1
-PROM_NS=$2
-TEST_NS=$3
+PROM_NS=$1
+TEST_NS=$2
 
 echo "Reverting deployments back to their original state for NS matching..."
 git checkout .
